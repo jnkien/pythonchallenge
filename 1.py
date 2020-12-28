9aeaa032dd4c7a33a0fa15aef3350d58139c6d1d
Jean-Noël KIEN <jnkien@gmail.com>
2020-12-28 15:08:06 +0100

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = ''.maketrans(intab, outtab)
+s.translate(trantab)
+
+f"http://www.pythonchallenge.com/pc/def/{'map'.translate(trantab)}.html"
